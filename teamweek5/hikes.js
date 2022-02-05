1c13984320f4a3010094f53632796e0c4c882f67
Tommy18062001 <johantony63@gmail.com>
2022-02-05 08:23:44 +0300

    Week 5 Update

--- teamweek5/hikes.js
+++ teamweek5/hikes.js
@@ -40,92 +40,92 @@ const hikeList = [
 const imgBasePath = "//byui-cit.github.io/cit261/examples/";
 export default class Hikes {
 constructor(elementId) {
-this.parentElement = document.getElementById(elementId);
-this.backButton = this.addBackButton(); // Call the funtion to create the button
+  this.parentElement = document.getElementById(elementId);
+  this.backButton = this.addBackButton(); // Call the funtion to create the button
 }
 
 // Return all the data from hikeList
 getAllHikes() {
-return hikeList;
+  return hikeList;
 }
 
 // Function to use to find a specific name inside the object and return the object
 getHikeByName(hikeName) {
-console.log(JSON.stringify(this.getAllHikes().find(hike => hike.name === hikeName))); // for checking if we got the right object
-return this.getAllHikes().find(hike => hike.name === hikeName);
+  console.log(JSON.stringify(this.getAllHikes().find(hike => hike.name === hikeName))); // for checking if we got the right object
+  return this.getAllHikes().find(hike => hike.name === hikeName);
 }
 
 // Function to display the list of hikes
 showHikeList() {
-this.parentElement.innerHTML = '';
-renderHikeList(this.parentElement, this.getAllHikes());
-this.addHikeListener();
+  this.parentElement.innerHTML = '';
+  renderHikeList(this.parentElement, this.getAllHikes());
+  this.addHikeListener();
 
-// Add class name hideButton to hide the button
-this.backButton.classList.add('hideButton');
+  // Add class name hideButton to hide the button
+  this.backButton.classList.add('hideButton');
 }
 
 // Function to display the full details of one hiking location
 showOneHike(hikeName) {
 
 const hike = this.getHikeByName(hikeName);
-this.parentElement.innerHTML = '';
-this.parentElement.appendChild(renderOneHikeFull(hike));
+  this.parentElement.innerHTML = '';
+  this.parentElement.appendChild(renderOneHikeFull(hike));
 
-// Remove the class name of the button to make the button visible
-this.backButton.classList.remove('hideButton')
+  // Remove the class name of the button to make the button visible
+  this.backButton.classList.remove('hideButton')
 }
 
 // Add the click event for each child element so that if the section is clicked it will run showOneHike() to display full details
 addHikeListener() {
-const listArray = Array.from(this.parentElement.children);
-listArray.forEach(child => {
-child.addEventListener('click', event => {
-this.showOneHike(event.currentTarget.dataset.name); // currentTarget returns whose event listener's callback is currently invoked
-console.log(event.currentTarget.dataset.name) // Test the data to be passed
-});
-});
+  const listArray = Array.from(this.parentElement.children);
+    listArray.forEach(child => {
+    child.addEventListener('click', event => {
+      this.showOneHike(event.currentTarget.dataset.name); // currentTarget returns whose event listener's callback is currently invoked
+      console.log(event.currentTarget.dataset.name) // Test the data to be passed
+    });
+  });
 }
 // Create the button at the end of the hike full details view and add event
 addBackButton() {
-const backButton = document.createElement("button");
-backButton.innerHTML = 'Go Back to All Hikes';
+  const backButton = document.createElement("button");
+  backButton.innerHTML = 'Go Back to All Hikes';
 
-// Add click event to the back button to display the original lists of hikes
-backButton.addEventListener('click', () => {
-this.showHikeList();
-});
+  // Add click event to the back button to display the original lists of hikes
+  backButton.addEventListener('click', () => {
+  this.showHikeList();
+  });
 
-// Add class name 'hideButton' to the button to hide it if the original lists of hikes view is displayed
-backButton.classList.add('hideButton');
-this.parentElement.after(backButton);
-return backButton;
+  // Add class name 'hideButton' to the button to hide it if the original lists of hikes view is displayed
+  backButton.classList.add('hideButton');
+  this.parentElement.after(backButton);
+  return backButton;
 }
 }
 
 // Function to compile each rendered li and display all of them in one page
 function renderHikeList(parent, hikes) {
-hikes.forEach(hike => {
-parent.appendChild(renderOneHike(hike));
-});
+  hikes.forEach(hike => {
+  parent.appendChild(renderOneHike(hike));
+  });
 }
 // Function to create the structure of the li element
 function renderOneHike(hike) {
-const item = document.createElement("li");
-item.setAttribute('data-name', hike.name); // set attribute to get the value of the name in addHikeListener()
-item.innerHTML = `<h2>${hike.name}</h2>
-<div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-<div>
-<div>
-<h3>Distance</h3>
-<p>${hike.distance}</p>
-</div>
-<div>
-<h3>Difficulty</h3>
-<p>${hike.difficulty}</p>
-</div>
-</div>` ;
-return item;
+  const item = document.createElement("li");
+  item.setAttribute('data-name', hike.name); // set attribute to get the value of the name in addHikeListener()
+  item.innerHTML = `<h2>${hike.name}</h2>
+  <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+  <div>
+  <div>
+  <h3>Distance</h3>
+  <p>${hike.distance}</p>
+  </div>
+  <div>
+  <h3>Difficulty</h3>
+  <p>${hike.difficulty}</p>
+  </div>
+  </div>` ;
+  return item;
 }
 
 // Function to create the structure the full view of the clicked hiking location
